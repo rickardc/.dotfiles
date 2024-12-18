036768adfe9201f8bbb8ade5ee10461568af52c0
Chris Rickard <103567821+rickardc@users.noreply.github.com>
2024-12-18 19:58:20 +1100

    182 current  2024-12-17 22:55:20  24.11.20241216.3945713  6.6.66                          *

--- nix/modules/ollama.nix
+++ nix/modules/ollama.nix
@@ -4,4 +4,16 @@
     acceleration = "rocm";
     rocmOverrideGfx = "10.3.1";
   };
+
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      openwebui = {
+        image = "ghcr.io/open-webui/open-webui:main";
+        ports = ["8080:8080"];
+        volumes = ["open-webui:/app/backend/data"];
+        extraOptions = ["--network=host"];
+      };
+    };
+  };
 }
