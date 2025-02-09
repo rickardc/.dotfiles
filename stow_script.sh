15bb61cca90d79126a71dfd029334fd01d01a620
Chris Rickard <103567821+rickardc@users.noreply.github.com>
2025-02-09 15:47:19 +1100

    al;ksdjfkl

--- stow_script.sh
+++ stow_script.sh
@@ -2,10 +2,5 @@
 
 #stow --dir=./.config --target=$HOME/.config
 
-for package in .config/*/; do  # Iterate over all directories (packages)
-  package="${package%/}"  # Remove the trailing slash
-  package="${package##*/}" # Remove the leading path
-  stow --dir=".config" --target="$HOME/.config" "$package" # Stow each package
-
-done
+stow --adopt --target="$HOME/.config" .config # Stow each package
 
